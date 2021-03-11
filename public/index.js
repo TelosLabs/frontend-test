8041c23e092378b7e98e6816a81159698742f188
Luis Angulo <luisf117@gmail.com>
2021-03-11 14:18:04 -0700

    Fixed js behavior and layout issues with arrows

--- public/index.js
+++ public/index.js
@@ -3,11 +3,11 @@ window.onload = () => {
   const collapsibleTextDivs =  d.getElementsByClassName("collapsible-element");
   for(const c of collapsibleTextDivs){
     c.addEventListener('click', (e) => {
-        // TODO: I think this could be better
         let arrow = c.getElementsByTagName("div")[0];
-        arrow.innerHTML = (arrow.innerHTML === "Λ") ? "v" : "Λ";
         let pElement = c.getElementsByTagName("p")[0];
-        pElement.style.display = (pElement.style.display === "none") ? "block" : "none";
+
+        arrow.innerHTML = (arrow.innerHTML === "V") ? "Λ" : "V";
+        pElement.classList.toggle("hide");
     });
   }
 };
